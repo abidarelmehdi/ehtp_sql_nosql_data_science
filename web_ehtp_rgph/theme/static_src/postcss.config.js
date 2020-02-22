3d951f2f4748dc6b234a83f21b019dbf503b158a
HOME LENOVO <abidar.mehdi95@gmail.com>
2020-02-22 14:15:02 +0100

    added purgecss and favicon

--- web_ehtp_rgph/theme/static_src/postcss.config.js
+++ web_ehtp_rgph/theme/static_src/postcss.config.js
@@ -1,7 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    '../../**/*.html'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss
   ]
 }
